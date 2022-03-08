40f77ede6548169a71e3f250701644a646fc50e7
Tim Schäfer <tim@gruber-schaefer.de>
2022-03-08 16:53:27 +0100

    separate PCA

--- exp/exp003_load_wahl_o_mat_2017.py
+++ exp/exp003_load_wahl_o_mat_2017.py
@@ -2,49 +2,28 @@
 
 data from https://github.com/gockelhahn/qual-o-mat-data
 """
-
-import json
-import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
 
 from src.data_utility.party_dictionary import PARTY_DICTIONARY
-from src.data_utility.wahl_o_mat.wahl_o_mat import opinion_to_array, load_opinion, load_statement
+from src.data_utility.wahl_o_mat.wahl_o_mat import opinion_to_array, load_opinion, load_statement, \
+    print_pca_significant_statements, perform_pca
 
+# load data
 dataframe_statement = load_statement()
 party_positions = opinion_to_array(load_opinion(n_parties=6))
 
-pca = PCA(n_components=2).fit(party_positions)
+# compute pca and new party positions
+party_positions, pca = perform_pca(party_positions)
+
+# print most significant statements
 print("pca explained variance ratio", pca.explained_variance_ratio_)
-pca_argsort = np.argsort(np.abs(pca.components_), axis=1)[:, ::-1]
-print("statements from first PCA")
-for statement in pca_argsort[0, :5]:
-    print(
-        statement,
-        dataframe_statement.iloc[statement]['label'],
-        ": ",
-        dataframe_statement.iloc[statement]['text'],
-        pca.components_[0, statement],
-    )
+print_pca_significant_statements(pca, dataframe_statement)
 
-print("\nstatements from second PCA")
-for statement in pca_argsort[1, :5]:
-    print(
-        statement,
-        dataframe_statement.iloc[statement]['label'],
-        ": ",
-        dataframe_statement.iloc[statement]['text'],
-        pca.components_[1, statement],
-    )
-
-plt.scatter(
-    party_positions.dot(pca.components_[0]),
-    party_positions.dot(pca.components_[1]),
-    )
+# plot party positions
+plt.scatter(party_positions[:, 0], party_positions[:, 1])
 for party in range(6):
-    plt.text(
-        party_positions.dot(pca.components_[0])[party],
-        party_positions.dot(pca.components_[1])[party],
-        PARTY_DICTIONARY[party],
-    )
+    plt.text(party_positions[party, 0],
+             party_positions[party, 1],
+             PARTY_DICTIONARY[party],
+             )
 plt.show()
--- src/data_utility/wahl_o_mat/wahl_o_mat.py
+++ src/data_utility/wahl_o_mat/wahl_o_mat.py
@@ -3,6 +3,7 @@ from os import path
 
 import numpy as np
 import pandas as pd
+from sklearn.decomposition import PCA
 
 filepath_wahl_o_mat = "../data/wahl_o_mat_2017"
 
@@ -37,3 +38,32 @@ def opinion_to_array(opinions: pd.DataFrame):
     for row in opinions.itertuples():
         party_positions[row.party, row.statement] = row.answer
     return party_positions
+
+
+def perform_pca(party_positions):
+    pca = PCA(n_components=2).fit(party_positions)
+    party_positions_pca = np.einsum("ps,cs->pc", party_positions, pca.components_)
+    return party_positions_pca, pca
+
+
+def print_pca_significant_statements(pca, dataframe_statement):
+    pca_argsort = np.argsort(np.abs(pca.components_), axis=1)[:, ::-1]
+    print("statements from first PCA")
+    for statement in pca_argsort[0, :5]:
+        print(
+            statement,
+            dataframe_statement.iloc[statement]['label'],
+            ": ",
+            dataframe_statement.iloc[statement]['text'],
+            pca.components_[0, statement],
+        )
+
+    print("\nstatements from second PCA")
+    for statement in pca_argsort[1, :5]:
+        print(
+            statement,
+            dataframe_statement.iloc[statement]['label'],
+            ": ",
+            dataframe_statement.iloc[statement]['text'],
+            pca.components_[1, statement],
+        )
