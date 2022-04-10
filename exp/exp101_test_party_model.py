d92abb447f77106cfba20816c954a04e40804cb2
Tim Schäfer <tim@gruber-schaefer.de>
2022-04-10 11:53:21 +0200

    [ADD] add time benchmark

--- exp/exp101_test_party_model.py
+++ exp/exp101_test_party_model.py
@@ -1,13 +1,20 @@
-"""Test the party model."""
+"""Test the party model.
+
+Additionally, measure the execution time as a benchmark case.
+"""
+from datetime import datetime
+
 import numpy as np
 
 from src.party_model.party_model import PartyModel
 
 # load party positions from file
 party_positions_pca = np.load("../data/saved/party_positions_pca.npy")
+print("party_positions_pca.shape", party_positions_pca.shape)
 
 # load voter positions from file
 voter_positions = np.load("../data/saved/voter_positions.npy")
+print("voter_positions.shape", voter_positions.shape)
 
 # initialize party model
 party_model = PartyModel(
@@ -15,12 +22,14 @@ party_model = PartyModel(
 )
 
 # plot initial party model
-# print(party_model.vote_share)
 party_model.scatter_plot()
 
-# perform one step in party model
+# perform 1000 steps in party model and measure execution time
+tic = datetime.now()
 for _ in range(100):
     party_model.step()
+toc = datetime.now()
+print(f"Execution time of 1000 steps: {toc-tic}")
 
-# plot party model after one step
+# plot party model afterwards
 party_model.scatter_plot()
