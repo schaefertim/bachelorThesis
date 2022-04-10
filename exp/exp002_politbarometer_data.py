b4a654540633f13adafbfde011cb6c643f1b3452
Tim Schäfer <tim@gruber-schaefer.de>
2022-04-10 16:09:56 +0200

    [REF] rename exp002

--- exp/exp002_politbarometer_data.py
+++
@@ -1,8 +0,0 @@
-"""Handling the data from Politbarometer."""
-
-from src.data_utility.politbarometer.politbarometer import load_voter_data
-
-dataframe = load_voter_data()
-
-print("no rows=", dataframe.count() + 1)
-print(dataframe.head(3))
